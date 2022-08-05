94fb26765424d3614bd5865cd39bd92371d3d7bd
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2022-08-05 16:43:09 +0100

    Grade point app.

---
+++ GradePoint.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GradePoint.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
